da6fff5634d20425096d3fe63734bd4ee4d65fd6
Tuan Ha <hatuan@email.cz>
2024-01-20 14:22:11 +0100

    change turns for players

--- script.js
+++ script.js
@@ -8,6 +8,29 @@ boxes.forEach(box => {
     box.addEventListener("click", () => {
         if(!isGameOver && box.innerHTML === "") {
             box.innerHTML = turn;
+
+            checkWin();
+            checkDraw();
+            changeTurn();
         }
     })
 })
+
+//Changing turns as in the Tic Tac Toe game, for better UI change background color on top
+function changeTurn() {
+    if (turn === "X") {
+        turn = "O";
+        document.querySelector(".bg").style.left = "85px";
+    } else {
+        turn = "X";
+        document.querySelector(".bg").style.left = "0px";
+    }
+}
+
+function checkWin() {
+
+}
+
+function checkDraw() {
+
+}
